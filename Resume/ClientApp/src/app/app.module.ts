612feaed3dfeab508d58f276ec19673d6f988d18
Eb0nheart <76154207+Eb0nheart@users.noreply.github.com>
2021-10-12 20:06:47 +0200

    Feature/navigation bar (#5)
    * removed boilerpalte and made the landing page
    
    * improved introduction page, close to done
    
    * Finished introduction page with ok responsiveness
    
    * Fixed styling for My Resume link on nav bar
    
    * Fixed navbar with new nav item
    
    * added cards for experience and education sections. Added OJ experience.
    
    * Finished education and experience page

--- Resume/ClientApp/src/app/app.module.ts
+++ Resume/ClientApp/src/app/app.module.ts
@@ -7,19 +7,22 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { ExperienceComponent } from './experience_and_education/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'experience', component: ExperienceComponent, pathMatch: 'full' }
     ])
   ],
   providers: [],
---
+++ Resume/ClientApp/src/app/experience_and_education/experience.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.css', '../base.css']
+})
+export class ExperienceComponent {
+}
